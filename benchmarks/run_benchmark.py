d74c00997e4f1eddf7c69c112be23d1364cb685e
Aaron Lam <achlam@uwaterloo.ca>
2018-11-14 15:24:15 -0500

    Fix some more bugs

--- benchmarks/run_benchmark.py
+++ benchmarks/run_benchmark.py
@@ -1,7 +1,9 @@
+import argparse
 import bitmath
 import subprocess
+import time
 
-import run_producer.run_producer_script as run_producer
+from run_producer import run_producer_script as run_producer
 
 CMD_TEMPLATE = "kafka-topics.sh {cmd} --zookeeper {zookeeper}  {options_str}"
 
@@ -31,12 +33,12 @@ if __name__ == "__main__":
     parser.add_argument("--topic", type=str, required=True)
     parser.add_argument("--record-size", type=str, required=True, help="e.g. 256B")
     parser.add_argument("--throughput", type=str, required=True, help="e.g. 10MB")
-    parser.add_argument("--time", type=int, required=True, help="60", 
-            help="time in seconds")
+    parser.add_argument("--time", type=int, required=True, 
+            help="time in seconds, e.g. 60")
     parser.add_argument("--output", type=str, required=True)
     parser.add_argument("--producer-config", type=str, required=True)
 
-    args.parser.parse_args()
+    args = parser.parse_args()
 
     throughput=int(bitmath.parse_string(args.throughput).to_Byte())
     record_size=int(bitmath.parse_string(args.record_size).to_Byte())
@@ -46,8 +48,10 @@ if __name__ == "__main__":
 
 
     delete_topic(args.zookeeper, args.topic)
+    time.sleep(10)
     create_topic(args.zookeeper, args.topic)
 
     run_producer(args.topic, throughput, record_size, total_records,
             args.producer_config, args.output)
 
+    delete_topic(args.zookeeper, args.topic)
