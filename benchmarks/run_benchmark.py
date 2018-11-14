5b09296dd1b8de19da61a6623608ad85cb529ced
Aaron Lam <achlam@uwaterloo.ca>
2018-11-14 14:55:01 -0500

    Script for running benchmarks

---
+++ benchmarks/run_benchmark.py
@@ -0,0 +1,53 @@
+import bitmath
+import subprocess
+
+import run_producer.run_producer_script as run_producer
+
+CMD_TEMPLATE = "kafka-topics.sh {cmd} --zookeeper {zookeeper}  {options_str}"
+
+CREATE_OPT_TEMPLATE = "--replication-factor {replication_factor} " + \
+                      "--partitions {partitions} " + \
+                      "--topic {topic}"
+DELETE_OPT_TEMPLATE = "--topic {topic}"
+
+def create_topic(zookeeper, topic, replication_factor=1, partitions=1):
+    options_str = CREATE_OPT_TEMPLATE.format(
+            replication_factor=replication_factor,
+            partitions=partitions,
+            topic=topic)
+    subprocess.call(CMD_TEMPLATE.format(cmd="--create",
+        zookeeper=zookeeper, 
+        options_str=options_str), 
+        shell=True)
+
+def delete_topic(zookeeper, topic):
+    options_str = DELETE_OPT_TEMPLATE.format(topic=topic)
+    subprocess.call(CMD_TEMPLATE.format(cmd="--delete", 
+        zookeeper=zookeeper,
+        options_str=options_str), shell=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topic", type=str, required=True)
+    parser.add_argument("--record-size", type=str, required=True, help="e.g. 256B")
+    parser.add_argument("--throughput", type=str, required=True, help="e.g. 10MB")
+    parser.add_argument("--time", type=int, required=True, help="60", 
+            help="time in seconds")
+    parser.add_argument("--output", type=str, required=True)
+    parser.add_argument("--producer-config", type=str, required=True)
+
+    args.parser.parse_args()
+
+    throughput=int(bitmath.parse_string(args.throughput).to_Byte())
+    record_size=int(bitmath.parse_string(args.record_size).to_Byte())
+
+    records_per_second = throughput / record_size
+    total_records = records_per_second * args.time
+
+
+    delete_topic(args.zookeeper, args.topic)
+    create_topic(args.zookeeper, args.topic)
+
+    run_producer(args.topic, throughput, record_size, total_records,
+            args.producer_config, args.output)
+
