5a72af566bb9523328e9d08a7432dc6d4fccc3b7
Aaron Lam <achlam@uwaterloo.ca>
2018-11-13 22:47:40 -0500

    Fix the math

--- benchmarks/run-producer.py
+++ benchmarks/run-producer.py
@@ -4,8 +4,10 @@ import subprocess
 import os
 
 CMD_TEMPLATE = "kafka-producer-perf-test.sh --topic {topic} " + \
-               "--throughput {throughput} --record-size {record_size} " + \
-               "--num-records {num_records} --producer.config {producer_config}"
+               "--throughput {records_per_second} " + \
+               "--record-size {record_size} " + \
+               "--num-records {total_records} " + \
+               "--producer.config {producer_config}"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--topic", type=str, required=True)
@@ -16,11 +18,17 @@ parser.add_argument("--producer-config", type=str, required=True)
 
 args = parser.parse_args()
 
+throughput=bitmath.parse_string(args.throughput).to_Byte()
+record_size=bitmath.parse_string(args.record_size).to_Byte()
+
+records_per_second = throughput / record_size
+total_records = records_per_second * 60
+
 with open(os.path.abspath(args.output), 'w') as output_file:
     subprocess.call(CMD_TEMPLATE.format(topic=args.topic,
-        throughput=int(bitmath.parse_string(args.throughput).to_Byte()),
-        record_size=int(bitmath.parse_string(args.record_size).to_Byte()),
-        num_records=args.num_records,
+        records_per_second=records_per_second,
+        record_size=record_size,
+        num_records=total_records,
         producer_config=os.path.abspath(args.producer_config), 
         stdout=output_file))
 
