d49166e5ada7761cd5eea4f48e32fd0e591c1793
Aaron Lam <achlam@uwaterloo.ca>
2018-11-23 14:48:53 -0500

    Remove the create and delete topic stuff

--- benchmarks/run_producer_benchmark.py
+++ benchmarks/run_producer_benchmark.py
@@ -1,11 +1,9 @@
 import argparse
-import bitmath
-import subprocess
 import os
-import time
-from datetime import datetime as dt
+import subprocess
 
-from run_producer import run_producer_script as run_producer
+import bitmath
+from .run_producer import run_producer_script as run_producer
 
 CMD_TEMPLATE = "kafka-topics.sh {cmd} --zookeeper {zookeeper}  {options_str}"
 
@@ -14,20 +12,22 @@ CREATE_OPT_TEMPLATE = "--replication-factor {replication_factor} " + \
                       "--topic {topic}"
 DELETE_OPT_TEMPLATE = "--topic {topic}"
 
+
 def create_topic(zookeeper, topic, replication_factor=1, partitions=1):
     options_str = CREATE_OPT_TEMPLATE.format(
-            replication_factor=replication_factor,
-            partitions=partitions,
-            topic=topic)
+        replication_factor=replication_factor,
+        partitions=partitions,
+        topic=topic)
     subprocess.call(CMD_TEMPLATE.format(cmd="--create",
-        zookeeper=zookeeper, 
-        options_str=options_str), shell=True)
+                                        zookeeper=zookeeper,
+                                        options_str=options_str), shell=True)
+
 
 def delete_topic(zookeeper, topic):
     options_str = DELETE_OPT_TEMPLATE.format(topic=topic)
-    subprocess.call(CMD_TEMPLATE.format(cmd="--delete", 
-        zookeeper=zookeeper,
-        options_str=options_str), shell=True)
+    subprocess.call(CMD_TEMPLATE.format(cmd="--delete",
+                                        zookeeper=zookeeper,
+                                        options_str=options_str), shell=True)
 
 
 if __name__ == "__main__":
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     parser.add_argument("--topic", type=str, required=True)
     parser.add_argument("--record-size", type=str, required=True, help="e.g. 256B")
     parser.add_argument("--throughput", type=str, required=True, help="e.g. 10MB")
-    parser.add_argument("--time", type=int, required=True, 
-            help="time in seconds, e.g. 60")
+    parser.add_argument("--time", type=int, required=True,
+                        help="time in seconds, e.g. 60")
     parser.add_argument("--instances", type=int, required=True)
     parser.add_argument("--output", type=str, required=True)
     parser.add_argument("--producer-config", type=str, required=True)
@@ -44,39 +44,26 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    throughput=int(bitmath.parse_string(args.throughput).to_Byte())
-    record_size=int(bitmath.parse_string(args.record_size).to_Byte())
+    throughput = int(bitmath.parse_string(args.throughput).to_Byte())
+    record_size = int(bitmath.parse_string(args.record_size).to_Byte())
 
     records_per_second = throughput / record_size
     total_records = records_per_second * args.time
 
-
-    create_topic(args.zookeeper, args.topic)
-
-    time.sleep(10)
-
     processes = []
 
-    print("Benchmark begins: {}".format(dt.now()))
-
     if args.instances == 1:
-        processes.append(run_producer(args.topic, throughput, record_size, 
-            total_records, args.producer_config, args.output))
+        processes.append(run_producer(args.topic, throughput, record_size,
+                                      total_records, args.producer_config, args.output))
     else:
         dir_path, basename = os.path.split(args.output)
         root, ext = os.path.splitext(basename)
         for i in range(args.instances):
             output_name = "{root}-{instance}{ext}".format(root=root,
-                    instance=i, ext=ext)
+                                                          instance=i, ext=ext)
             output_path = os.path.join(dir_path, output_name)
-            processes.append(run_producer(args.topic, throughput, record_size, 
-                total_records, args.producer_config, output_path))
+            processes.append(run_producer(args.topic, throughput, record_size,
+                                          total_records, args.producer_config, output_path))
 
     for p in processes:
         p.wait()
-
-    print("Benchmark finished: {}".format(dt.now()))
-
-    delete_topic(args.zookeeper, args.topic)
-
-    time.sleep(10)
