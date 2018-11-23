9dbbb305b5e1e5e5996e249c07f84d2b3e00fe84
Aaron Lam <achlam@uwaterloo.ca>
2018-11-23 14:49:05 -0500

    update requirements

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -13,14 +13,18 @@ BROKERS_FILE = 'brokers'
 CONSUMERS_FILE = 'consumers'
 PRODUCERS_FILE = 'producers'
 
-CMD_TEMPLATE = "kafka-topics.sh {cmd} --zookeeper {zookeeper}  {options_str}"
+KAKFA_TOPICS_CMD_TEMPLATE = "kafka-topics.sh {cmd} --zookeeper {zookeeper}  {options_str}"
 
 CREATE_OPT_TEMPLATE = "--replication-factor {replication_factor} " + \
                       "--partitions {partitions} " + \
                       "--topic {topic}"
 DELETE_OPT_TEMPLATE = "--topic {topic}"
 
+VMSTAT_START_CMD = 'vmstat -n -t -S M 1 3600 > {path}'
+
+
 BENCHMARK_TOPIC = 'benchmark-topic'
+BENCHMARK_LENTH = '300' # 5 minutes
 
 
 def get_hostnames(filename):
@@ -38,14 +42,16 @@ def open_ssh(hostname):
     return client
 
 
-def mkdir_benchmark_results(num_replicas, num_producers, num_consumers, producer_throughput):
-    # TODO generalize for not just producer throughput
+def mkdir_benchmark_results(num_replicas, num_producers, num_consumers, producer_throughput, trial):
+    # TODO: generalize for not just producer throughput
     mkdir_path = os.path.join(DATA_DIR, '{}-replicas'.format(num_replicas),
                               '{}-producers'.format(num_producers), '{}-consumers'.format(num_consumers),
-                              '{}-throughput'.format(producer_throughput))
+                              '{}-throughput'.format(producer_throughput), '{}-trial'.format(trial))
     print('Making directory... {}'.format(mkdir_path))
 
     os.makedirs(mkdir_path, exist_ok=True)
+
+    return mkdir_path
 
 
 def create_topic(zookeeper, topic, replication_factor=1, partitions=1):
@@ -53,30 +59,58 @@ def create_topic(zookeeper, topic, replication_factor=1, partitions=1):
         replication_factor=replication_factor,
         partitions=partitions,
         topic=topic)
-    subprocess.call(CMD_TEMPLATE.format(cmd="--create",
-                                        zookeeper=zookeeper,
-                                        options_str=options_str), shell=True)
+    subprocess.call(KAKFA_TOPICS_CMD_TEMPLATE.format(cmd="--create",
+                                                     zookeeper=zookeeper,
+                                                     options_str=options_str), shell=True)
 
 
 def delete_topic(zookeeper, topic):
     options_str = DELETE_OPT_TEMPLATE.format(topic=topic)
-    subprocess.call(CMD_TEMPLATE.format(cmd="--delete",
-                                        zookeeper=zookeeper,
-                                        options_str=options_str), shell=True)
+    subprocess.call(KAKFA_TOPICS_CMD_TEMPLATE.format(cmd="--delete",
+                                                     zookeeper=zookeeper,
+                                                     options_str=options_str), shell=True)
 
 
-def run_producer_throughput_trial(zookeeper, num_replicas, num_producers, num_consumers, producer_throughput):
+def start_vmstats(hosts, data_dir):
+    channels = []
+
+    for host in hosts:
+        client = open_ssh(host)
+        stdin, stdout, stderr = client.exec_command(VMSTAT_START_CMD.format(data_dir))
+        channels.append(stdout.channel)
+
+    for channel in channels:
+        exit_status = channel.recv_exit_status()
+        client.close()
+
+
+def stop_vmstats(hosts):
+    channels = []
+
+    for host in hosts:
+        client = open_ssh(host)
+        stdin, stdout, stderr = client.exec_command('pkill -f vmstat')
+        channels.append(stdout.channel)
+
+    for channel in channels:
+        exit_status = channel.recv_exit_status()
+        client.close()
+
+
+def run_producer_throughput_trial(zookeeper, trial, num_replicas, producers, num_consumers, producer_throughput):
     # create test result directory
-    mkdir_benchmark_results(num_replicas, num_producers, num_consumers, producer_throughput)
+    data_dir = mkdir_benchmark_results(num_replicas, len(producers), num_consumers, producer_throughput, trial)
 
     # create kafka topic
     create_topic(zookeeper, BENCHMARK_TOPIC, replication_factor=num_replicas)
 
     # start vmstat
+    start_vmstats(producers, data_dir)
 
     time.sleep(20)
 
     # end vmstat
+    stop_vmstats(producers)
 
     # delete kafka topic
     delete_topic(zookeeper, BENCHMARK_TOPIC)
@@ -94,5 +128,5 @@ if __name__ == '__main__':
     print('consumers: {}'.format(consumers))
     print('producers: {}'.format(producers))
 
-    run_producer_throughput_trial(zookeepers[0], 1, 1, 1, 5)
+    run_producer_throughput_trial(zookeepers[0], 0, 1, 1, 1, 5)
 
