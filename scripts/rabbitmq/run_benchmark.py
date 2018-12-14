8e107ae5a477e0827a1a4b617b8af19b2f21273c
Aaron Lam <aaron.chihon.lam@gmail.com>
2018-12-14 17:18:47 -0500

    Run another trial

--- scripts/rabbitmq/run_benchmark.py
+++ scripts/rabbitmq/run_benchmark.py
@@ -22,7 +22,7 @@ EXCHANGE_NAME = 'benchmark-exchange'
 EXCHANGE_TYPE = 'fanout'
 QUEUE_PATTERN = 'benchmark-queue-%d'
 RECORD_SIZE = '512B'
-TOTAL_RECORDS = 1000000
+TOTAL_RECORDS = 100000
 
 SSH_NODE_PY_CMD_TEMPLATE = '''
 cd kafka-benchmarks/;
@@ -216,10 +216,10 @@ def run_trial(trial_num, nodes, consumers, producers, consumer_instances, produc
 
 
 def run_experiments(nodes, consumers, producers):
-    start_throughput = 1
-    end_throughput = 3
-    step_throughput = 1
-    num_trials = 2
+    start_throughput = 35
+    end_throughput = 55
+    step_throughput = 5
+    num_trials = 1
     for throughput in range(start_throughput, end_throughput, step_throughput):
         print("\n========= RUNNING EXPERIMENT! ============\n")
         print("number of nodes: {}".format(len(nodes)))
