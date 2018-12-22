9a03d18313fb0ea2b2a426a4903eb9490cf28606
Aaron Lam <achlam@uwaterloo.ca>
2018-12-21 19:50:03 -0500

    update graphing script

--- scripts/rabbitmq/process_producer_output.py
+++ scripts/rabbitmq/process_producer_output.py
@@ -12,6 +12,7 @@ import scipy
 import scikits.bootstrap as bootstrap
 
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'rabbitmq'))
+PERSISTENCE_DIR = os.path.join(DATA_DIR, 'persistent')
 
 THROUGHPUT_REGEX = r'(\d+) msg/s'
 PRODUCER_REGEX = r'^producer-tem\d+(-\d+)*\.txt$'
@@ -106,17 +107,37 @@ def read_throughputs(data_dir):
                     return pandas.read_csv(os.path.join(root, file))
 
 
-def unpack_throughput_data(replicas):
+def unpack_throughput_data(throughput_data, replicas):
     means = sorted(throughput_data[replicas], key=lambda t: t[0])
     return zip(*means)
+
+
+def get_persistent_data(data_dir):
+    throughput_data = defaultdict(list)
+    for replicas in range(2, 4):
+        for throughput in range(1, 10):
+            throughput_filepath = filepath_to_experiment(data_dir, replicas, 1, 1, throughput)
+            # process_trials(throughput_filepath)
+            # process_trial_throughputs(throughput_filepath)
+            df = read_throughputs(throughput_filepath)
+            df.fillna(0)
+
+            dfs[replicas][throughput] = df
+
+            low, high = confidence_interval(df, 'throughput_consumer')
+
+            throughput_data[replicas].append((df['throughput_producer'].mean(), df['throughput_consumer'].mean(),
+                                              low, high))
+
+    return throughput_data
 
 
 if __name__ == '__main__':
     dfs = defaultdict(dict)
 
     throughput_data = defaultdict(list)
-    for replicas in range(1, 4):
-        for throughput in range(5, 55, 5):
+    for replicas in range(2, 4):
+        for throughput in range(1, 15):
             throughput_filepath = filepath_to_experiment(DATA_DIR, replicas, 1, 1, throughput)
             # process_trials(throughput_filepath)
             # process_trial_throughputs(throughput_filepath)
@@ -130,28 +151,30 @@ if __name__ == '__main__':
             throughput_data[replicas].append((df['throughput_producer'].mean(), df['throughput_consumer'].mean(),
                                               low, high))
 
+    persistent_data = get_persistent_data(PERSISTENCE_DIR)
+
     fig, ax = plt.subplots()
 
-    for replicas in range(1, 4):
+    for replicas in range(2, 4):
         means = sorted(throughput_data[replicas], key=lambda t: t[0])
         producers, consumers, low, high = zip(*means)
 
-    producers, consumers, low, high = unpack_throughput_data(1)
-    ax.errorbar(producers, consumers, yerr=(low, high),
-                fmt='-o',
-                color='#5DADE2',
-                barsabove=True,
-                ecolor='k',
-                capsize=2,
-                capthick=2,
-                elinewidth=2,
-                markersize=8,
-                #markeredgewidth=1,
-                #markeredgecolor='k',
-                label='replication-factor=1')
+    # producers, consumers, low, high = unpack_throughput_data(1)
+    # ax.errorbar(producers, consumers, yerr=(low, high),
+    #             fmt='-o',
+    #             color='#5DADE2',
+    #             barsabove=True,
+    #             ecolor='k',
+    #             capsize=2,
+    #             capthick=2,
+    #             elinewidth=2,
+    #             markersize=8,
+    #             #markeredgewidth=1,
+    #             #markeredgecolor='k',
+    #             label='replication-factor=1, persistent=False')
 
-    producers, consumers, low, high = unpack_throughput_data(2)
-    plt.errorbar(producers, consumers, yerr=(low, high),
+    producers, consumers, low, high = unpack_throughput_data(throughput_data, 2)
+    ax.errorbar(producers[:7] + producers[-1:], consumers[:7] + consumers[-1:], yerr=(low[:7] + low[-1:], high[:7] + high[-1:]),
                  fmt='-^',
                  barsabove=True,
                  color='#58D68D',
@@ -162,10 +185,34 @@ if __name__ == '__main__':
                  markersize=8,
                  #markeredgewidth=1,
                  #markeredgecolor='k',
-                 label='replication-factor=2')
+                 label='persistent=False')
 
-    producers, consumers, low, high = unpack_throughput_data(3)
-    plt.errorbar(producers, consumers, yerr=(low, high),
+    producers, consumers, low, high = unpack_throughput_data(persistent_data, 2)
+    ax.errorbar(producers[:3] + producers[-1:], consumers[:3] + consumers[-1:], yerr=(low[:3] + low[-1:], high[:3] + high[-1:]),
+                 fmt='-o',
+                 barsabove=True,
+                 color='#633974',
+                 ecolor='k',
+                 capsize=2,
+                 capthick=2,
+                 elinewidth=2,
+                 markersize=8,
+                 #markeredgewidth=1,
+                 #markeredgecolor='k',
+                 label='persistent=True')
+
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(left=0)
+    ax.set_title('Replication Factor of 2')
+    ax.set_xlabel('Producer Throughput (MB/s)')
+    ax.set_ylabel('Consumer Throughput (MB/s)')
+    ax.legend()
+    fig.savefig('replicas-2.eps')
+
+    fig, ax = plt.subplots()
+
+    producers, consumers, low, high = unpack_throughput_data(throughput_data, 3)
+    ax.errorbar(producers[:7] + producers[-1:], consumers[:7] + consumers[-1:], yerr=(low[:7] + low[-1:], high[:7] + high[-1:]),
                  fmt='-s',
                  color='#EB984E',
                  barsabove=True,
@@ -176,14 +223,26 @@ if __name__ == '__main__':
                  markersize=8,
                  # markeredgewidth=1,
                  # markeredgecolor='k',
-                 label='replication-factor=3')
+                 label='persistent=False')
 
-    plt.ylim(bottom=0)
-    plt.xlim(left=0)
-    plt.title('producers=1, consumers=1, queues=1, record-size=512 bytes')
+    producers, consumers, low, high = unpack_throughput_data(persistent_data, 3)
+    ax.errorbar(producers[:3] + producers[-1:], consumers[:3] + consumers[-1:], yerr=(low[:3] + low[-1:], high[:3] + high[-1:]),
+                 fmt='-s',
+                 color='#1F618D',
+                 barsabove=True,
+                 ecolor='k',
+                 capsize=2,
+                 capthick=2,
+                 elinewidth=2,
+                 markersize=8,
+                 # markeredgewidth=1,
+                 # markeredgecolor='k',
+                 label='persistent=True')
+
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(left=0)
+    ax.set_title('Replication Factor of 3')
     ax.set_xlabel('Producer Throughput (MB/s)')
     ax.set_ylabel('Consumer Throughput (MB/s)')
-    plt.legend()
-    plt.draw()
-    plt.show()
-
+    ax.legend()
+    fig.savefig('replicas-3.eps')
